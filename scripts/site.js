05e356df1e164d4933596490c34ee218fa15e7e2
Fanie Reynders <fanie@reynders.co>
2014-12-23 18:45:26 +0100

    Fixed offcanvas side bar

--- scripts/site.js
+++ scripts/site.js
@@ -1,4 +1,10 @@
-$('pre.scissr-syntax code').each(function () {
-    $(this).html($(this).html().replace(/(\,|\:|\'|\*|\@|\(|\)|\=|xml|json)/g, '<span class="scissr-operator">$1</span>'));
-    $(this).html($(this).html().replace(/(string|number|float|date|bool)/g, '<span class="scissr-type">$1</span>'));
+$(function () {
+	$('pre.scissr-syntax code').each(function () {
+	   	$(this).html($(this).html().replace(/(\,|\:|\'|\*|\@|\(|\)|\=|xml|json)/g, '<span class="scissr-operator">$1</span>'));
+	    	$(this).html($(this).html().replace(/(string|number|float|date|bool)/g, '<span class="scissr-type">$1</span>'));
+	});
+
+  	$('[data-toggle="offcanvas"]').click(function () {
+   		$('.row-offcanvas').toggleClass('active')
+  	});
 });
